3be7cc9df71d4a068f7091e4dbfd553a259781c6
Rois Faozi <roisfaozi55@gmail.com>
2020-07-02 17:14:56 +0700

    Penambahan Activity baru, Perpindahan Intent Dengan Data

--- app/src/main/java/com/kotlin/myintentnewapp/MainActivity.kt
+++ app/src/main/java/com/kotlin/myintentnewapp/MainActivity.kt
@@ -14,6 +14,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveActivity : Button = findViewById(R.id.btn_move_activity)
         btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveActivityWithData : Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveActivityWithData.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
@@ -21,6 +24,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.btn_move_activity -> {
                 val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 startActivity(moveIntent)
+            }
+
+            R.id.btn_move_activity_data -> {
+                val moveActivityWithData = Intent(this@MainActivity, MoveActivityWithData::class.java)
+                moveActivityWithData.putExtra(MoveActivityWithData.EXTRA_NAME, "DicodingACADEMY Boy")
+                moveActivityWithData.putExtra(MoveActivityWithData.EXTRA_AGE, 5)
+                startActivity(moveActivityWithData)
+            }
         }
     }
 }
